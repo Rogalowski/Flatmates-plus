61b56e0c7530ce9c0a137b17467809505a5ad76e
Jacek Mateusz Rogowski <rogalowski@gmail.com>
2023-02-14 19:41:45 +0100

    Added Navigation

---
+++ flatemates-plus/src/components/TheNavigation.vue
@@ -0,0 +1,24 @@
+<script>
+</script>
+
+<template>
+<header>
+
+<div class="home">
+    <div id="nav" class="wrapper">
+        <router-link to="/">Home</router-link>
+        <router-link to="/about">About Dest</router-link>
+        <router-link to="/myview">MyView</router-link>
+    </div>
+</div>
+
+</header>
+  <!-- <div class="container" >
+        <router-view/>
+    </div> -->  <RouterView />
+</template>
+
+
+<style scoped>
+
+</style>
