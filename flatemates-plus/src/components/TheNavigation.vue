9041417131792e9c8eb9ebc63718a33b72c78f19
Jacek Mateusz Rogowski <rogalowski@gmail.com>
2023-02-14 20:20:44 +0100

    add some content expenses and calendar

--- flatemates-plus/src/components/TheNavigation.vue
+++ flatemates-plus/src/components/TheNavigation.vue
@@ -1,4 +1,5 @@
 <script>
+import favicon from "../../public/favicon.ico"
 </script>
 
 <template>
@@ -6,16 +7,19 @@
 
 <div class="home">
     <div id="nav" class="wrapper">
+        <router-link  to="/">  <img src="../../public/favicon.ico"  /> </router-link>
+
         <router-link to="/">Home</router-link>
-        <router-link to="/about">About Dest</router-link>
-        <router-link to="/myview">MyView</router-link>
+        <router-link to="/expenses">Expenses</router-link>
+        <router-link to="/calendar">Calendar</router-link>
+        <router-link to="/about">About Flatemates+</router-link>
     </div>
 </div>
 
 </header>
-  <!-- <div class="container" >
+    <div class="container" >
         <router-view/>
-    </div> -->  <RouterView />
+    </div>
 </template>
 
 
--- flatemates-plus/src/views/Calendar.vue
+++ flatemates-plus/src/views/Calendar.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="about">
-    <h1>This is an About Flatemates+ page</h1>
+    <h1>This is an Calendar page</h1>
   </div>
 </template>
 
